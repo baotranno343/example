e1abf4e86f69f071e70bcc4de05d63fa34f63498
baotranno343 <81433617+baotranno343@users.noreply.github.com>
2022-08-07 12:23:01 +0700

    Update about.js

--- about.js
+++ about.js
@@ -1,4 +1,4 @@
 // console.log(increase()()); // output still: 1
-Co Gi Dau Ma Phai Khoc
+Co Gi Dau Ma Phai Khoc Sua File ne
 // const useClosure = increase();
 // console.log(useClosure()); // output: 1
